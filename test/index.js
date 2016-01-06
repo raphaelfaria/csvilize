113c967ead35b333b07d4e99d174bb40227fe41b
Raphael Faria <raphaelfmf@gmail.com>
2016-01-06 00:52:45 -0200

    Changes name to csvilize

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 // import assert from 'assert';
 // import csvSchema from '../lib';
 
-// describe('csv-schema', function () {
+// describe('csvilize', function () {
 //   it('should have unit test!', function () {
 //     assert(false, 'we expected this package author to add actual unit tests.');
 //   });
