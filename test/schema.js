66717efec1179e30b7e36786b43efad0e76443d7
Raphael Faria <raphaelfmf@gmail.com>
2016-01-06 19:11:19 -0200

    Adds more tests to Schema

--- test/schema.js
+++ test/schema.js
@@ -33,4 +33,33 @@ describe('Schema', () => {
       assert.throws(() => newSchema(schema), Error);
     });
   });
+
+  describe('methods', () => {
+    let schema;
+
+    beforeEach(() => {
+      schema = new Schema({
+        str: String,
+        num: {
+          type: Number,
+          default: 123,
+        },
+      });
+    });
+
+    it('should get the default value for a property', () => {
+      assert.equal(schema.getDefault('str'), null);
+      assert.equal(schema.getDefault('num'), 123);
+    });
+
+    it('should get type of property', () => {
+      assert.equal(schema.getType('str'), String);
+      assert.equal(schema.getType('num'), Number);
+    });
+
+    it('should get type name of property', () => {
+      assert.equal(schema.getTypeName('str'), 'String');
+      assert.equal(schema.getTypeName('num'), 'Number');
+    });
+  });
 });
