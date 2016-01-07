cc24d0e0a48c5aef080c23daf53437ec9f11cde0
Raphael Faria <raphaelfmf@gmail.com>
2016-01-06 23:23:57 -0200

    Adds SchemaType

--- test/schema.js
+++ test/schema.js
@@ -40,16 +40,28 @@ describe('Schema', () => {
     beforeEach(() => {
       schema = new Schema({
         str: String,
-        num: {
-          type: Number,
-          default: 123,
+        num: Number,
+        bool: Boolean,
+        date: Date,
+        model: Model.compile('Test'),
+        detailed: {
+          type: String,
+          default: 'blabla',
         },
       });
     });
 
     it('should get the default value for a property', () => {
       assert.equal(schema.getDefault('str'), null);
-      assert.equal(schema.getDefault('num'), 123);
+      assert.equal(schema.getDefault('num'), null);
+      assert.equal(schema.getDefault('bool'), false);
+
+      const date = Math.floor(Date.now() / 1000);
+      const defaultDate = Math.floor(schema.getDefault('date') / 1000);
+
+      assert.equal(date, defaultDate);
+
+      assert.equal(schema.getDefault('detailed'), 'blabla');
     });
 
     it('should get type of property', () => {
